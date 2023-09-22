89c0ad5b16a7fd2f0de6c96125a827546614ff5b
Chiptune93 <eoen012@gmail.com>
2023-09-23 07:54:31 +0900

    :sparkles:

--- Cogs/remind.py
+++ Cogs/remind.py
@@ -1,30 +1,37 @@
 import os
-import datetime
+from datetime import datetime
 from discord.ext import tasks, commands
 from dotenv import load_dotenv
+import pytz
 import requests
 import random
 
+# 채널 정보를 환경 변수에서 세팅
 target_channel = os.getenv('DISCORD_REMIND_CHANNEL')
 
 
+# Cog 클래스
 class Remind(commands.Cog):
+    # 생성자, 봇을 받아 초기화함.
     def __init__(self, bot):
         self.bot = bot
         self.channels = [target_channel]  # 채널 ID 리스트로 변경
 
+    # 반복 작업 구문
     @tasks.loop(minutes=60)  # 매 1시간 마다 반복.
     async def send_messages_to_channels(self):
         # 날짜 체크.
-        dt = datetime.datetime.now()
+        KST = pytz.timezone('Asia/Seoul')
+        dt = datetime.now().astimezone(KST)
 
         # 오전 9시 ~ 오후 9시 사이에만 실행한다.
-        print(f'현재 시간 : {dt}')
+        print(f'현재 시간 : {dt} -> {dt.hour} : {dt.minute} : {dt.second}')
         if (dt.hour == 9 and dt.minute >= 0 and dt.second >= 0) and (
                 dt.hour < 22 and dt.minute <= 59 and dt.second <= 59):
 
             for channel_id in self.channels:
                 try:
+                    # 채널에 메세지를 전송한다.
                     channel = self.bot.get_channel(channel_id)
                     await channel.send(make_message(get_notion_data()))
                 except Exception as e:
@@ -33,10 +40,12 @@ class Remind(commands.Cog):
         else:
             print('Remind 시간이 아닙니다.')
 
+    # 작업 실행 전, 봇 실행이 완료될 때 까지 기다린다.
     @send_messages_to_channels.before_loop
     async def before_send_messages_to_channels(self):
         await self.bot.wait_until_ready()
 
+    # Cog Listener, 준비가 완료되면 작업을 시작한다.
     @commands.Cog.listener()
     async def on_ready(self):
         print(f'{self.bot.user} has connected to Discord!')
@@ -46,10 +55,12 @@ class Remind(commands.Cog):
         self.send_messages_to_channels.cancel()
 
 
+# Cog 셋업 함수
 async def setup(app):
     await app.add_cog(Remind(app))
 
 
+# 데이터 -> 메세지 세팅 함수
 def make_message(row):
     message = '**# ' + row['subject'] + '**'
     message += '\n>>> ' + row['info'] + '\n\n'
@@ -57,6 +68,7 @@ def make_message(row):
     return message
 
 
+# 데이터를 노션에서 가져오는 함수
 def get_notion_data():
     t = os.getenv('NOTION_CONNECTION_TOKEN')
     b = "https://api.notion.com/v1/databases/"
--- reminerbot.py
+++ reminerbot.py
@@ -5,24 +5,29 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
+# 환경 변수 로딩
 load_dotenv()
 
+# 디스코드 봇 토큰 값 지정
 token = os.getenv('DISCORD_BOT_TOKEN')
 
+# 디스코드 봇 인텐트 및 봇 초기화
 intents = discord.Intents.all()
 bot = commands.Bot(intents=intents, command_prefix='!')
 
-
+# Cogs 모듈 로딩을 위한 구문, 파일을 통해 구분하여 가져옴
+# 이렇게 하면, 폴더 내부에 모듈 넣어서 관리할 수 있어 편함.
 async def load_extensions():
     for filename in os.listdir("Cogs"):
         if filename.endswith(".py"):
             await bot.load_extension(f"Cogs.{filename[:-3]}")
 
-
+# 메인 함수 호출
+# 디스코드 특정 버전(12++) 부터 async를 적용해야 제대로 호출이 가능함.
 async def main():
     async with bot:
         await load_extensions()
         await bot.start(token)
 
-
+# asyncio를 통해 봇 구동.
 asyncio.run(main())
